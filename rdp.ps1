89ec0b8dcd9418416e9ae9565b8f358b13c3b7f8
User <user@HRK1-LMC-F73344.local>
2020-05-12 14:33:36 +0300

    cert config; collection creation

--- rdp.ps1
+++ rdp.ps1
@@ -1,3 +1,15 @@
+$rebootCount=6
+
+#
+# Variables
+#
+
+$Secure_String = ConvertTo-SecureString "ns_g1l0h" -AsPlainText -Force
+$DNSName = "rds-9000.local"
+$NetbiosName = $DNSName.substring(0,$DNSName.lastindexof("."))
+$NetbiosName = $NetbiosName.toupper()
+$serverName="$env:computername.$DNSName"
+
 
 Install-WindowsFeature -Name "AD-Domain-Services" -IncludeAllSubFeature
 Install-WindowsFeature -Name "RSAT-AD-Tools" -IncludeAllSubFeature
@@ -6,15 +18,15 @@ Install-WindowsFeature -Name "RSAT-AD-Tools" -IncludeAllSubFeature
 # AD DS Deployment
 #
 
-$Secure_String = ConvertTo-SecureString "ns_g1l0h" -AsPlainText -Force
+
 
 Import-Module ADDSDeployment
 Install-ADDSForest `
 -CreateDnsDelegation:$false `
 -DatabasePath "C:\Windows\NTDS" `
 -DomainMode "WinThreshold" `
--DomainName "rds-9000.com" `
--DomainNetbiosName "RDS-9000" `
+-DomainName $DNSName `
+-DomainNetbiosName $NetbiosName `
 -ForestMode "WinThreshold" `
 -InstallDns:$true `
 -LogPath "C:\Windows\NTDS" `
@@ -29,15 +41,51 @@ Install-ADDSForest `
 #
 
 Install-WindowsFeature -Name "RDS-Connection-Broker"
-Install-WindowsFeature -Name "RDS-Gateway"
-Install-WindowsFeature -Name "RDS-Licensing"
 Install-WindowsFeature -Name "RDS-RD-Server"
 Install-WindowsFeature -Name "RDS-Web-Access"
 restart-computer
-
-[string]$serverName="$env:computername.rds-9000.com"
-
 New-RDSessionDeployment -ConnectionBroker $serverName -SessionHost $serverName -WebAccessServer $serverName
+
+Install-WindowsFeature -Name "RDS-Licensing"
 Add-RDServer -Server $serverName -Role RDS-LICENSING -ConnectionBroker $serverName
-Add-RDServer -Server $serverName -Role RDS-LICENSING -ConnectionBroker $serverName -GatewayExternalFqdn $serverName
-restart-computer
+
+Install-WindowsFeature -Name "RDS-Gateway"
+Add-RDServer -Server $serverName -Role RDS-LICENSING -ConnectionBroker $serverName -GatewayExternalFqdn $DNSName
+
+
+# Creating self-signed certificate
+
+new-selfsignedcertificate -certstorelocation cert:\localmachine\my -dnsname $DNSName -FriendlyName "rds-local"|Export-PfxCertificate -FilePath C:\rds-local.pfx -Password $Secure_String
+
+# Certificate configuration
+
+Set-RDCertificate -Role RDGateway `
+                  -ImportPath C:\rds-local.pfx `
+                  -Password $Secure_String `
+                  -ConnectionBroker $serverName `
+                  -Force
+
+Set-RDCertificate -Role RDWebAccess `
+                  -ImportPath C:\rds-local.pfx `
+                  -Password $Secure_String `
+                  -ConnectionBroker $serverName `
+                  -Force
+
+Set-RDCertificate -Role RDPublishing `
+                  -ImportPath C:\rds-local.pfx `
+                  -Password $Secure_String `
+                  -ConnectionBroker $serverName `
+                  -Force
+
+Set-RDCertificate -Role RDRedirector `
+                  -ImportPath C:\rds-local.pfx `
+                  -Password $Secure_String `
+                  -ConnectionBroker $serverName `
+                  -Force
+
+# Creating Collection
+
+New-RDSessionCollection -CollectionName Desktop `
+                        -CollectionDescription "Desktop Publication" `
+                        -SessionHost $serverName `
+                        -ConnectionBroker $serverName
